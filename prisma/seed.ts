d7fa0b320a1ce779d263677a6aba087155d2d340
Leon <leepingho603@gmail.com>
2025-02-24 14:54:20 +0800

    Update - Add user database model and seed some datas

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,8 +6,16 @@ const prisma = new PrismaClient();
 async function main() {
   // Delete existing records (optional, use with caution)
   await prisma.product.deleteMany({});
+  await prisma.account.deleteMany({});
+  await prisma.session.deleteMany({});
+  await prisma.verificationToken.deleteMany({});
+  await prisma.user.deleteMany({});
 
   await prisma.product.createMany({ data: sampleData.products });
+  // await prisma.account.createMany({ data: sampleData.accounts });
+  // await prisma.session.createMany({ data: sampleData.sessions });
+  // await prisma.verificationToken.createMany({ data: sampleData.verificationTokens });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Seeding complete!");
 }
