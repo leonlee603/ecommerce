58df2e4698c20fc15ce4edf48ea477d5dfb91b08
Leon Lee <leepingho603@gmail.com>
2025-03-02 22:33:59 +0800

    Update - Cart page

--- src/app/(root)/cart/CartTable.tsx
+++ src/app/(root)/cart/CartTable.tsx
@@ -5,7 +5,7 @@ import { useTransition } from 'react';
 import { toast } from 'sonner';
 import Link from 'next/link';
 import Image from 'next/image';
-import { ArrowRight, Loader, Minus, Plus } from 'lucide-react';
+import { ArrowRight, Minus, Plus } from 'lucide-react';
 import {
   Table,
   TableBody,
@@ -14,10 +14,12 @@ import {
   TableRow,
   TableCell,
 } from '@/components/ui/table';
+import { Card, CardContent } from '@/components/ui/card';
 
 import { Cart } from '../../../../types';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { Button } from '@/components/ui/button';
+import { formatCurrency } from '@/lib/utils';
 
 export default function CartTable({ cart }: { cart?: Cart }) {
   const router = useRouter();
@@ -31,7 +33,7 @@ export default function CartTable({ cart }: { cart?: Cart }) {
           Cart is empty. <Link href="/">Go Shopping</Link>
         </div>
       ) : (
-        <div className="grid md-grid-cols-4 md:gap-5">
+        <div className="grid md:grid-cols-4 md:gap-5">
           <div className="overflow-x-auto md:col-span-3">
             <Table>
               <TableHeader>
@@ -101,6 +103,20 @@ export default function CartTable({ cart }: { cart?: Cart }) {
               </TableBody>
             </Table>
           </div>
+          <Card>
+            <CardContent className="p-4 gp-4">
+              <div className="pd-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className="font-bold">
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+              <Button className="w-full" disabled={isPending} onClick={() => startTransition(() => router.push("/shipping-address"))}>
+                {/* {isPending ? (<Loader className="w-4 h-4 animate-spin"/>) : (<ArrowRight className="w-4 h-4" />)} */}
+                <ArrowRight className="w-4 h-4" /> Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
