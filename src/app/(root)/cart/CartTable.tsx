08dce9c8c49bca20422c76284bb0de6c720b782d
Leon <leepingho603@gmail.com>
2025-03-03 10:24:17 +0800

    Update - Clean up

--- src/app/(root)/cart/CartTable.tsx
+++ src/app/(root)/cart/CartTable.tsx
@@ -27,7 +27,7 @@ export default function CartTable({ cart }: { cart?: Cart }) {
 
   return (
     <>
-      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      <h1 className="h2-bold py-4">Shopping Cart</h1>
       {!cart || cart.items.length === 0 ? (
         <div>
           Cart is empty. <Link href="/">Go Shopping</Link>
@@ -104,16 +104,16 @@ export default function CartTable({ cart }: { cart?: Cart }) {
             </Table>
           </div>
           <Card>
-            <CardContent className="p-4 gp-4">
-              <div className="pd-3 text-xl">
+            <CardContent className="gap-4 p-4">
+              <div className="pb-3 text-xl">
                 Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
                 <span className="font-bold">
                   {formatCurrency(cart.itemsPrice)}
                 </span>
               </div>
               <Button className="w-full" disabled={isPending} onClick={() => startTransition(() => router.push("/shipping-address"))}>
-                {/* {isPending ? (<Loader className="w-4 h-4 animate-spin"/>) : (<ArrowRight className="w-4 h-4" />)} */}
-                <ArrowRight className="w-4 h-4" /> Proceed to Checkout
+                {/* {isPending ? (<Loader className="h-4 w-4 animate-spin"/>) : (<ArrowRight className="h-4 w-4" />)} */}
+                <ArrowRight className="h-4 w-4" /> Proceed to Checkout
               </Button>
             </CardContent>
           </Card>
