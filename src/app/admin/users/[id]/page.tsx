3cca369c4cf02f94058c09d6c97e0635cc834767
Leon <leepingho603@gmail.com>
2025-03-21 17:58:39 +0800

    Update - delete and update user by admin

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,9 +1,30 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import UpdateUserForm from "./UpdateUserForm";
+import { getUserById } from "@/lib/actions/user.actions";
 import { requireAdmin } from "@/lib/auth-guard";
 
-export default async function page() {
+export const metadata: Metadata = {
+  title: 'Update User',
+};
+
+export default async function page(props: {
+  params: Promise<{
+    id: string;
+  }>;
+}) {
   await requireAdmin();
-  
+
+  const { id } = await props.params;
+
+  const user = await getUserById(id);
+
+  if (!user) notFound();
+
   return (
-    <div>page</div>
-  )
+    <div className="mx-auto max-w-lg space-y-8">
+      <h1 className="h2-bold">Update User</h1>
+      <UpdateUserForm user={user} />
+    </div>
+  );
 }
