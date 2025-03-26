a46fe904859699fd26b2b48adaf32effe99ec31b
Leon <leepingho603@gmail.com>
2025-03-26 16:56:38 +0800

    Update - Add category drawer to header

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 
 import Menu from "./Menu";
 import { APP_NAME } from "@/lib/constants";
-
+import CategoryDrawer from "./CategoryDrawer";
 
 export default function Header() {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/images/logo.svg"
               alt={`${APP_NAME} logo`}
@@ -18,9 +19,9 @@ export default function Header() {
               height={48}
               priority
             />
-            <div className="span ml-3 hidden text-2xl font-bold lg:block">
+            <span className="ml-3 hidden text-2xl font-bold lg:block">
               {APP_NAME}
-            </div>
+            </span>
           </Link>
         </div>
         <Menu />
